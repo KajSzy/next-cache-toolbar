27b0dad5c4ac18761c1b712ff184f671d8f99961
Kajetan Szymczak <kajtszymczak@gmail.com>
2025-01-23 16:43:27 +0100

    Fix next-14 build

--- apps/next-14/src/app/toolbar.tsx
+++ apps/next-14/src/app/toolbar.tsx
@@ -2,5 +2,6 @@ import { NextCacheToolbar } from "next-cache-toolbar";
 import "next-cache-toolbar/style.css";
 
 export default function Toolbar() {
+	// @ts-expect-error async component is not supported in React 18
 	return <NextCacheToolbar />;
 }
